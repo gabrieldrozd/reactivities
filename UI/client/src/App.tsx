f4009a4084d8c58d0b95410c94709e73fdd9339b
Gabriel <drozd.gj@gmail.com>
2022-02-23 14:59:38 +0100

    Axios added, example using of hooks added to App.tsx

--- UI/client/src/App.tsx
+++ UI/client/src/App.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+    const [activities, setActivities] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/activities').then(response => {
+            console.log(response);
+            setActivities(response.data);
+        })
+    }, []);
+
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <p>My first React App</p>
-                <span>Dupa Sraczka i kaczka</span>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+
+                <ul>
+                    {activities.map((activity: any) => (
+                        <li key={activity.id}>
+                            <span>{activity.title}</span>
+                        </li>
+                    ))}
+                </ul>
             </header>
         </div>
     );
