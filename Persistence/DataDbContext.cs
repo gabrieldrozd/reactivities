857d289b39f4363da720d8e3a3b65082bf18ad6c
Gabriel <drozd.gj@gmail.com>
2022-03-04 01:36:23 +0100

    Added part of authorization with basic configuration of services

--- Persistence/DataDbContext.cs
+++ Persistence/DataDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence;
 
-public class DataDbContext : DbContext
+public class DataDbContext : IdentityDbContext<AppUser>
 {
     public DataDbContext(DbContextOptions options) : base(options)
     {
