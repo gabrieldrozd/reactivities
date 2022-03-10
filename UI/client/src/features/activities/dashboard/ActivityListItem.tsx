3ccaffc42da91d9352effb3ca8b83c88882e3e1d
Gabriel <drozd.gj@gmail.com>
2022-03-10 17:27:44 +0100

    small changes to upload user image from client

--- UI/client/src/features/activities/dashboard/ActivityListItem.tsx
+++ UI/client/src/features/activities/dashboard/ActivityListItem.tsx
@@ -11,6 +11,8 @@ interface Props {
 
 export default function ActivityListItem({activity}: Props) {
 
+
+
     return (
         <Segment.Group>
             <Segment>
@@ -21,14 +23,17 @@ export default function ActivityListItem({activity}: Props) {
                 }
                 <Item.Group>
                     <Item>
-                        <Item.Image size="tiny" circular src="/assets/user.png"
-                                    style={{marginBottom: 3}}
-                        />
+                        <Item.Image size="tiny" circular src={activity.host?.image || "/assets/user.png"}/>
                         <Item.Content>
                             <Item.Header as={Link} to={`/activities/${activity.id}`}>
                                 {activity.title}
                             </Item.Header>
-                            <Item.Description>Hosted by {activity.host?.displayName}</Item.Description>
+                            <Item.Description>
+                                Hosted by
+                                <Link to={`/profiles/${activity.host?.userName}`}>
+                                    {activity.host?.displayName}
+                                </Link>
+                            </Item.Description>
                             {activity.isHost && (
                                 <Item.Description>
                                     <Label basic color='orange'>
