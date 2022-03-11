d7b2903dc5279469408a3a6ee5a3a5b26e624f03
Gabriel <drozd.gj@gmail.com>
2022-03-11 01:13:18 +0100

    Photo management on client side finished

--- UI/client/src/app/api/agent.ts
+++ UI/client/src/app/api/agent.ts
@@ -4,7 +4,7 @@ import {toast} from "react-toastify";
 import {history} from "../../index";
 import {store} from "../stores/store";
 import {User, UserFormValues} from "../models/user";
-import {Profile} from "../models/profile";
+import {Photo, Profile} from "../models/profile";
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -83,7 +83,16 @@ const Account = {
 }
 
 const Profiles = {
-    get: (userName: string) => requests.get<Profile>(`/profiles/${userName}`)
+    get: (userName: string) => requests.get<Profile>(`/profiles/${userName}`),
+    uploadPhoto: (file: Blob) => {
+        let formData = new FormData();
+        formData.append('File', file);
+        return axios.post<Photo>('photos', formData, {
+            headers: {'Content-type': 'multipart/form-data'}
+        })
+    },
+    setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
+    deletePhoto: (id: string) => requests.del(`/photos/${id}`)
 }
 
 const agent = {
--- UI/client/src/features/profiles/ProfilePhotos.tsx
+++ UI/client/src/features/profiles/ProfilePhotos.tsx
@@ -1,7 +1,7 @@
-import React, {useState} from "react";
+import React, {SyntheticEvent, useState} from "react";
 import {observer} from "mobx-react-lite";
 import {Button, Card, Grid, Header, Image, Tab} from "semantic-ui-react";
-import { Profile } from "../../app/models/profile";
+import {Photo, Profile} from "../../app/models/profile";
 import {useStore} from "../../app/stores/store";
 import PhotoUploadWidget from "../../app/common/imageUpload/PhotoUploadWidget";
 
@@ -10,8 +10,23 @@ interface Props {
 }
 
 export default observer(function ProfilePhotos({profile}: Props) {
-	const {profileStore: {isCurrentUser}} = useStore();
+	const {profileStore: {isCurrentUser, uploadPhoto, uploading, loading, setMainPhoto, deletePhoto}} = useStore();
 	const [addPhotoMode, setAddPhotoMode] = useState(false);
+	const [target, setTarget] = useState('');
+
+	function handlePhotoUpload(file: Blob) {
+		uploadPhoto(file).then(() => setAddPhotoMode(false));
+	}
+
+	function handleSetMainPhoto(photo: Photo, e: SyntheticEvent<HTMLButtonElement>) {
+		setTarget(e.currentTarget.name);
+		setMainPhoto(photo);
+	}
+
+	function handleDeletePhoto(photo: Photo, e: SyntheticEvent<HTMLButtonElement>) {
+		setTarget(e.currentTarget.name);
+		deletePhoto(photo);
+	}
 
 	return (
 		<Tab.Pane>
@@ -27,12 +42,32 @@ export default observer(function ProfilePhotos({profile}: Props) {
 				</Grid.Column>
 				<Grid.Column width={16}>
 					{addPhotoMode ? (
-						<PhotoUploadWidget/>
+						<PhotoUploadWidget uploadPhoto={handlePhotoUpload} loading={uploading}/>
 					) : (
 						<Card.Group itemsPerRow={5}>
 							{profile.photos?.map(photo => (
 								<Card key={photo.id}>
 									<Image src={photo.url}/>
+									{isCurrentUser && (
+										<Button.Group fluid widths={2}>
+											<Button basic
+													color='green'
+													content='Main'
+													name={'main' + photo.id}
+													disabled={photo.isMain}
+													loading={target === 'main' + photo.id && loading}
+													onClick={e => handleSetMainPhoto(photo, e)}
+											/>
+											<Button basic
+													color='red'
+													icon='trash'
+													name={photo.id}
+													disabled={photo.isMain}
+													loading={target === photo.id && loading}
+													onClick={e => handleDeletePhoto(photo, e)}
+											/>
+										</Button.Group>
+									)}
 								</Card>
 							))}
 						</Card.Group>
